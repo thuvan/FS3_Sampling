e13dd4ea4354017794ecaad3989bebbeb13d2b29
thuvan <van01031990@gmail.com>
2016-04-26 14:32:58 +0900

    add script for convert text file between window & linux

---
+++ dataset/linux2win.bat
@@ -0,0 +1,20 @@
+ECHO OFF
+IF "%~1"=="" GOTO help
+IF "%~2"=="" GOTO help
+
+ECHO BEGIN CONVERTING TEXT FILE FROM LINUX FILE TO WINDOW FORMAT ...
+ECHO SOURCE FILE:  %1
+ECHO DEST FILE:  %2
+
+TR "\r" "\n" < %1 > %2
+
+ECHO FINISHED. PRESS ENTER TO EXIT.
+PAUSE
+GOTO end
+
+:help
+ECHO Wrong parameters.
+ECHO %0  <source_file>  <dest_file>
+PAUSE
+
+:end
